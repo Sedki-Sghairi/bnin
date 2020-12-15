8abcf1f8e5a433d9c3c4e776ad34f48e96c03b18
Sedki-Sghairi <sedkisghairi3@gmail.com>
2020-12-15 13:52:15 +0400

    lifting state to MainComponent

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import './App.css';
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import { Menu } from './menu'
-import DisplayMenu from './components/DisplayMenu';
+import Main from './components/MainComponent';
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      menu : Menu
-    }
-  }
   render() {
     return (
-      <>
-        <div className="bg-dark d-flex flex-column flex-md-row justify-content-center
-         align-items-center text-center black-lives-matter">
-         <span className="text-light mr-3 h4">Black Lives Matter.</span>
-         <div className='text-white'>We Support the Equal Justice Initiative.</div></div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">BNIN</NavbarBrand>
-          </div>
-        </Navbar>
-        <DisplayMenu menu = {this.state.menu} />
-      </>
+     <Main/>
     );
   }
 }
---
+++ src/components/MainComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { Menu } from '../menu'
+import DisplayMenu from './DisplayMenu';
+class Main extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      menu : Menu
+    }
+  }
+  render() {
+    return (
+      <>
+        <div className="bg-dark d-flex flex-column flex-md-row justify-content-center
+         align-items-center text-center black-lives-matter">
+         <span className="text-light mr-3 h4">Black Lives Matter.</span>
+         <div className='text-white'>We Support the Equal Justice Initiative.</div></div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">BNIN</NavbarBrand>
+          </div>
+        </Navbar>
+        <DisplayMenu menu = {this.state.menu} />
+      </>
+    );
+  }
+}
+export default Main;
