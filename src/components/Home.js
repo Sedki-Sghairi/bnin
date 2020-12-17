470b8b5788c44b418e86f09139ae0bebe8bc2957
Sedki-Sghairi <sedkisghairi3@gmail.com>
2020-12-17 18:52:35 +0400

    home images

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,21 +5,21 @@ const items = [
     {
       src: 'images/1.jpg',
       altText: 'Slide 1',
-      caption: 'We support our local community',
-      header: 'All Our Food is made by  Fresh Local Ingredients',
+      caption: 'We take our customers feedback very seriously, and we only offer what they label "delicious"',
+      header: 'Delicious Is Not just Our Brand Name',
       key: '1'
     },
     {
       src: 'images/2.jpg',
-      altText: 'Slide 2',
-      caption: 'Slide 2',
-      header: 'Slide 2 Header',
+      altText: 'All Our Food is made by  Fresh Local Ingredients',
+      caption: 'All Our Food is made by  Fresh Local Ingredients',
+      header: 'We support our local community',
       key: '2'
     },
     {
       src: 'images/3.jpg',
-      caption: 'Slide 3',
-      header: 'Slide 3 Header',
+      caption: 'Give yourself a treat by trying our signature breakfast',
+      header: 'Our Chef Special',
       key: '3'
     }
   ];
@@ -27,7 +27,7 @@ const items = [
 
 export default function Home() {
     return (
-        <div className="neg-mar">
+        <div className="container-fluid">
             <UncontrolledCarousel items={items} />
         </div>
     )
--- src/menu.js
+++ src/menu.js
@@ -5,6 +5,7 @@ export const Menu = [
 		category: 'breakfast',
 		price: 15.99,
 		img: './images/item-1.jpeg',
+		featured: true,
 		desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `
 	},
 	{
@@ -12,6 +13,7 @@ export const Menu = [
 		title: 'avocado sunshine',
 		category: 'lunch',
 		price: 13.99,
+		featured: false,
 		img: './images/item-2.jpeg',
 		desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `
 	},
@@ -19,6 +21,7 @@ export const Menu = [
 		id: 3,
 		title: 'godzilla milkshake',
 		category: 'shakes',
+		featured: false,
 		price: 6.99,
 		img: './images/item-3.jpg',
 		desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`
@@ -27,6 +30,7 @@ export const Menu = [
 		id: 21,
 		title: 'strawberry paradise',
 		category: 'shakes',
+		featured: false,
 		price: 5.99,
 		img: './images/item-17.jpg',
 		desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`
@@ -35,6 +39,7 @@ export const Menu = [
 		id: 4,
 		title: 'country delight',
 		category: 'breakfast',
+		featured: true,
 		price: 20.99,
 		img: './images/item-4.jpeg',
 		desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `
@@ -43,6 +48,7 @@ export const Menu = [
 		id: 5,
 		title: '500g rib eye steak',
 		category: 'lunch',
+		featured: false,
 		price: 22.99,
 		img: './images/item-5.jpg',
 		desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `
@@ -51,6 +57,7 @@ export const Menu = [
 		id: 16,
 		title: '500g rib eye steak',
 		category: 'dinner',
+		featured: false,
 		price: 22.99,
 		img: './images/item-19.jpg',
 		desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `
@@ -59,6 +66,7 @@ export const Menu = [
 		id: 6,
 		title: 'oreo dream',
 		category: 'shakes',
+		featured: false,
 		price: 18.99,
 		img: './images/item-6.jpeg',
 		desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`
@@ -67,6 +75,7 @@ export const Menu = [
 		id: 15,
 		title: 'signature soup',
 		category: 'lunch',
+		featured: true,
 		price: 10.99,
 		img: './images/item-15.jpg',
 		desc: 'Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday'
@@ -75,6 +84,7 @@ export const Menu = [
 		id: 17,
 		title: 'signature soup',
 		category: 'dinner',
+		featured: false,
 		price: 10.99,
 		img: './images/item-20.jpg',
 		desc: 'Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday'
@@ -83,6 +93,7 @@ export const Menu = [
 		id: 7,
 		title: 'bacon overflow',
 		category: 'breakfast',
+		featured: false,
 		price: 8.99,
 		img: './images/item-7.jpeg',
 		desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `
@@ -91,6 +102,7 @@ export const Menu = [
 		id: 8,
 		title: 'salmon fillet',
 		category: 'dinner',
+		featured: false,
 		price: 12.99,
 		img: './images/item-8.jpeg',
 		desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `
@@ -99,6 +111,7 @@ export const Menu = [
 		id: 18,
 		title: 'salmon fillet',
 		category: 'lunch',
+		featured: false,
 		price: 12.99,
 		img: './images/item-8.jpeg',
 		desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `
@@ -107,6 +120,7 @@ export const Menu = [
 		id: 9,
 		title: 'quarantine buddy',
 		category: 'shakes',
+		featured: false,
 		price: 16.99,
 		img: './images/item-9.jpeg',
 		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
@@ -115,6 +129,7 @@ export const Menu = [
 		id: 10,
 		title: 'cappuccino',
 		category: 'breakfast',
+		featured: false,
 		price: 2,
 		img: './images/item-13.jpeg',
 		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
@@ -123,6 +138,7 @@ export const Menu = [
 		id: 11,
 		title: 'espresso',
 		category: 'breakfast',
+		featured: false,
 		price: 1.5,
 		img: './images/item-14.jpeg',
 		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
@@ -131,6 +147,7 @@ export const Menu = [
 		id: 20,
 		title: 'breakfast tea',
 		category: 'breakfast',
+		featured: false,
 		price: 2.5,
 		img: './images/item-16.jpeg',
 		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
@@ -139,6 +156,7 @@ export const Menu = [
 		id: 12,
 		title: 'club sandwich',
 		category: 'dinner',
+		featured: false,
 		price: 11.5,
 		img: './images/item-12.jpeg',
 		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
@@ -147,6 +165,7 @@ export const Menu = [
 		id: 13,
 		title: 'our bnina salad',
 		category: 'dinner',
+		featured: false,
 		price: 9.5,
 		img: './images/item-21.jpg',
 		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
@@ -155,6 +174,7 @@ export const Menu = [
 		id: 19,
 		title: 'our mahboula salad',
 		category: 'lunch',
+		featured: false,
 		price: 9.5,
 		img: './images/item-22.jpg',
 		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
@@ -163,6 +183,7 @@ export const Menu = [
 		id: 14,
 		title: '200g steak',
 		category: 'lunch',
+		featured: false,
 		price: 9.5,
 		img: './images/item-10.jpeg',
 		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
@@ -171,6 +192,7 @@ export const Menu = [
 		id: 22,
 		title: '200g steak',
 		category: 'dinner',
+		featured: false,
 		price: 9.5,
 		img: './images/item-18.jpg',
 		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
