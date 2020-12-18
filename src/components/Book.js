0bc7a607ee303a2b9824878a6fec1b2ea81b3fd8
Sedki-Sghairi <sedkisghairi3@gmail.com>
2020-12-18 23:11:44 +0400

    booking form styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -12,67 +12,67 @@ export default function Book() {
                 </Breadcrumb>
             </div>
             <h1 className="text-primary text-center mb-3">  Please fill out this Form</h1>
-            <div className="row">
+            <div className="row" id='book'>
             <form>
-            <div class="form-row">
-              <label for="name" class="col-md-2 col-form-label">Name</label>
-              <div class="col-md-10">
-              <input type="text" id="name" name="firstname" placeholder="Full Name" class="form-control"/>
+            <div className="form-row">
+              <label htmlFor="name" className="col-md-2 col-form-label">Name</label>
+              <div className="col-md-10">
+              <input type="text" id="name" name="firstname" placeholder="First Name" className="form-control"/>
               </div>
             </div>
-            <div class="form-row">
-                <label for="petname" class="col-md-2 col-form-label">Pet Name</label>
-                <div class="col-md-5">
-                <input type="text" id="petname" name="petname" placeholder="Pet Name" class="form-control"/>
+            <div className="form-row">
+                <label htmlFor="lastname" className="col-md-2 col-form-label">Last Name</label>
+                <div className="col-md-10">
+                <input type="text" id="lastname" name="lastname" placeholder="last Name" className="form-control"/>
                 </div>
-                <div class="col-md-5">
-                    <select name="pet" id="pet" class="form-control">
-                        <option>Cat</option>
-                        <option>Dog</option>
-                        <option>Bird</option>
+            </div>
+            <div className="form-row">
+                <label htmlFor="table" className="offset-sm-2 col-md-5 col-form-label">Table For:</label>
+                <div className="col col-md">
+                <select name="table" id="table" className="form-control">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
                     </select>
                 </div>
             </div>
-            <div class="form-row">
-                <label for="telnum" class="col-12 col-md-2 col-form-label">tel.</label>
-                <div class="col-5 col-md-3">
-                  <input type="tel" name="areacode" id="areacode" class="form-control" placeholder="Area Code"/>
+            <div className="form-row">
+                <label htmlFor="telnum" className="col-12 col-md-2 col-form-label">tel.</label>
+                <div className="col-5 col-md-3">
+                  <input type="tel" name="areacode" id="areacode" className="form-control" placeholder="Area Code"/>
                 </div>
-                <div class="col-7 col-md-7">
-                  <input type="tel" name="telnum" id="telnum" class="form-control" placeholder="Tel. Number"/>
-                </div>
-            </div>
-            <div class="form-row">
-                <label for="emailid" class="col-md-2 col-form-label">Email</label>
-                <div class="col-md-10">
-                    <input type="email" id="emailid" name="emailid" placeholder="Email" class="form-control"/>
+                <div className="col-7 col-md-7">
+                  <input type="tel" name="telnum" id="telnum" className="form-control" placeholder="Tel. Number"/>
                 </div>
             </div>
-            <div class="form-row">
-                <label for="address" class="col-md-2 col-form-label">Address</label>
-                <div class="col-md-10">
-                    <input type="text" id="address" name="address" placeholder="Address" class="form-control"/>
+            <div className="form-row">
+                <label htmlFor="emailid" className="col-md-2 col-form-label">Email</label>
+                <div className="col-md-10">
+                    <input type="email" id="emailid" name="emailid" placeholder="Email" className="form-control"/>
                 </div>
             </div>
-            <div class="form-row">
-              <label for="date" class="col-12 col-md-2 col-form-label">Date</label>
-              <div class="col-5 col-md-5">
-                <input type="date" name="date" id="date" class="form-control"/>
+            <div className="form-row">
+              <label htmlFor="date" className="col-12 col-md-2 col-form-label">Date</label>
+              <div className="col-5 col-md-5">
+                <input type="date" name="date" id="date" className="form-control"/>
               </div>
-              <div class="col-5 col-md-5">
-                <input type="time" name="time" id="time" class="form-control"/>
+              <div className="col-5 col-md-5">
+                <input type="time" name="time" id="time" className="form-control"/>
               </div>
             </div>
-            <div class="form-row">
-                <label for="feedback" class="col-md-2 col-form-label">Optional</label>
+            <div className="form-row">
+                <label htmlFor="feedback" className="col-md-2 col-form-label">Special Info.</label>
 
-                <div class="col-md-10">
-                  <textarea class="form-control" id="feedback" name="feedback" rows="10" placeholder="Medications/Allergies/Specific instructions"></textarea>
+                <div className="col-md-10">
+                  <textarea className="form-control" id="feedback" name="feedback" rows="6" placeholder="Special Occasion"></textarea>
                 </div>
             </div>
-            <div class="form-row">
-                <div class="col-md-10 offset-md-2">
-                    <button type="submit" class="btn btn-primary">
+            <div className="form-row">
+                <div className="col-md-10 offset-md-2">
+                    <button type="submit" className="btn btn-primary">
                         Submit Booking
                     </button>
                 </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,26 +3,26 @@ import { Link } from 'react-router-dom';
 
 function Footer(props) {
     return(
-    <div class="footer py-5 mb-0">
-        <div class="container">
-            <div class="row justify-content-center text-center mb-3">             
-                <div class="col-12 col-md-6  align-self-center">
-                <i class="fa fa-phone fa-lg"></i>: +971 5567 3367<br/>
-                <i class="fa fa-envelope"></i>: <a href="mailto:delicious@delicious.io" class="text-white">delicious@delicious.io</a>
+    <div className="footer py-5 mb-0">
+        <div className="container">
+            <div className="row justify-content-center text-center mb-3">             
+                <div className="col-12 col-md-6  align-self-center">
+                <i className="fa fa-phone fa-lg"></i>: +971 5567 3367<br/>
+                <i className="fa fa-envelope"></i>: <a href="mailto:delicious@delicious.io" className="text-white">delicious@delicious.io</a>
                 </div>
-                <div class="col-12 col-md-6  align-self-center">
-                <div class="text-center ">
-                    <h6 class="follow-us">follow us</h6>
-                        <a class="btn btn-social-icon  btn-google" href="#0"><i class="fa fa-google-plus fa-lg"></i></a>
-                        <a class="btn btn-social-icon  btn-facebook" href="#0"><i class="fa fa-facebook fa-lg"></i></a>
-                        <a class="btn btn-social-icon  btn-linkedin" href="#0"><i class="fa fa-linkedin fa-lg"></i></a>
-                        <a class="btn btn-social-icon  btn-twitter" href="#0"><i class="fa fa-twitter fa-lg"></i></a>
-                        <a class="btn btn-social-icon  btn-google" href="#0"><i class="fa fa-youtube fa-lg"></i></a>
+                <div className="col-12 col-md-6  align-self-center">
+                <div className="text-center ">
+                    <h6 className="follow-us">follow us</h6>
+                        <a className="btn btn-social-icon  btn-google" href="#0"><i className="fa fa-google-plus fa-lg"></i></a>
+                        <a className="btn btn-social-icon  btn-facebook" href="#0"><i className="fa fa-facebook fa-lg"></i></a>
+                        <a className="btn btn-social-icon  btn-linkedin" href="#0"><i className="fa fa-linkedin fa-lg"></i></a>
+                        <a className="btn btn-social-icon  btn-twitter" href="#0"><i className="fa fa-twitter fa-lg"></i></a>
+                        <a className="btn btn-social-icon  btn-google" href="#0"><i className="fa fa-youtube fa-lg"></i></a>
                     </div>
                 </div>
             </div>
-            <div class="row justify-content-center mt-2">             
-                <div class="col-auto ">
+            <div className="row justify-content-center mt-2">             
+                <div className="col-auto ">
                     <p>© Copyright 2020 Delicious</p>
                 </div>
             </div>
